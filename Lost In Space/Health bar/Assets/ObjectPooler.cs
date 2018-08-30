d2a576013f6e162ec0d3f80ee93f688a0355e06a
Shreyash Shrivastava <shreyash0023@gmail.com>
2018-08-29 22:40:36 -0500

    Health bar

---
+++ Lost In Space/Health bar/Assets/ObjectPooler.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+
+
+    }
+
+    #region singletom
+
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+	// Use this for initialization
+	void Start () {
+
+
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj =  Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+
+
+            }
+
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public GameObject SpawnFromPool (string tage, Vector3 position, Quaternion rotation)
+ 
+    {
+
+        if (!poolDictionary.ContainsKey(tage))
+        {
+            Debug.LogWarning("Pool with tag " + tag + "does not exist");
+            return null;
+               
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+
+    }
+}
